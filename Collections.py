3d8472b9085ec731ce7e34b74b04b84afb7fdfb2
Mage29 <100523328+Mage29@users.noreply.github.com>
2022-03-21 01:43:54 +0800

    Add files via upload

---
+++ Collections.py
@@ -0,0 +1,14 @@
+hartaKarun = ["emas",24,2.5,True]
+print(hartaKarun)
+
+#cara mengupdate nilai
+hartaKarun[1] = 20
+print(hartaKarun)
+
+#cara menghapus nilai
+hartaKarun. remove(20)
+print(hartaKarun)
+
+#cara menambahkan nilai
+hartaKarun. append(20)
+print(hartaKarun)
